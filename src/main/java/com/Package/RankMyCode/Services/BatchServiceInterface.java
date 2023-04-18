6dfe2f34f89c56b0faa1231e6bd644360f81b059
zipArk99 <kajiwalashaurya29@gmail.com>
2023-04-19 00:02:08 +0530

    commit

--- src/main/java/com/Package/RankMyCode/Services/BatchServiceInterface.java
+++ src/main/java/com/Package/RankMyCode/Services/BatchServiceInterface.java
@@ -4,6 +4,8 @@ import com.Package.RankMyCode.Collections.Batch;
 import java.util.List;
 import java.util.Optional;
 
+//this is batch service interface
+
 public interface BatchServiceInterface {
 
     Optional<Batch> getBatchById(String id);
